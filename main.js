07596dc1c9857daa99bc4fbd064bbc4cc1380f12
Swift <swiftx37@gmail.com>
2023-09-25 20:00:54 +0200

    Updating gradient direction and output code

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const colorPickers = document.querySelectorAll('.pickers input')
 const previewContainer = document.querySelector('.preview-container')
+const directionSelect = document.getElementById('direction')
+const outputTextarea = document.getElementById('outputCode')
 
 const generateGradient = () => {
-	const gradient = `linear-gradient(to top right, ${colorPickers[0].value}, ${colorPickers[1].value})`
+	const gradient = `linear-gradient(${directionSelect.value}, ${colorPickers[0].value}, ${colorPickers[1].value})`
 	console.log(gradient)
 	previewContainer.style.background = gradient
+	outputTextarea.textContent = `background: ${gradient};`
 }
 
 colorPickers.forEach(picker =>
 	picker.addEventListener('input', generateGradient)
 )
+
+directionSelect.addEventListener('change', generateGradient)
