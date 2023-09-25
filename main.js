2e3c08a3ed0775fb2e4df87e8151aaeece81b594
Swift <swiftx37@gmail.com>
2023-09-25 20:37:37 +0200

    Create a random gradient and copy the code buttons

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const colorPickers = document.querySelectorAll('.pickers input')
 const previewContainer = document.querySelector('.preview-container')
 const directionSelect = document.getElementById('direction')
 const outputTextarea = document.getElementById('outputCode')
+const refreshBtn = document.querySelector('.refresh-button')
+const copyBtn = document.querySelector('.copy-button')
 
-const generateGradient = () => {
+const generateGradient = isRandom => {
+	if (isRandom) {
+		colorPickers[0].value = getRandomColor()
+		colorPickers[1].value = getRandomColor()
+	}
 	const gradient = `linear-gradient(${directionSelect.value}, ${colorPickers[0].value}, ${colorPickers[1].value})`
 	console.log(gradient)
 	previewContainer.style.background = gradient
@@ -11,7 +17,20 @@ const generateGradient = () => {
 }
 
 colorPickers.forEach(picker =>
-	picker.addEventListener('input', generateGradient)
+	picker.addEventListener('input', () => generateGradient(false))
 )
 
-directionSelect.addEventListener('change', generateGradient)
+directionSelect.addEventListener('change', () => generateGradient(false))
+
+const getRandomColor = () => {
+	const hexColor = Math.floor(Math.random() * 0xffffff).toString(16)
+	return '#' + hexColor
+}
+
+refreshBtn.addEventListener('click', () => generateGradient(true))
+
+copyBtn.addEventListener('click', () => {
+	navigator.clipboard.writeText(outputTextarea.textContent)
+	copyBtn.textContent = 'Code has copied'
+	setTimeout(() => (copyBtn.textContent = 'Copy Code'), 1500)
+})
